e6b56c40dbd00b837249982cd5a2e61ebb81780f
RoxieMind <votuantay@gmail.com>
2024-08-07 19:19:27 +0200

    Test

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
+from kivy.config import ConfigParser
 from kivy.app import App
 from kivy.uix.widget import Widget
+from plyer import filechooser
 
 
 class GifMenu(Widget):
-    pass
 
+    config = ConfigParser(name="GifPlayerApp")
+
+
+    def choose_dir(self):
+        path = filechooser.choose_dir()
+        
+        self.config.set("folder", value = path)
+
+
+    def launch(self):
+        print(self.config.get("folder"))
 
 class GifPlayerApp(App):
     def build(self):
